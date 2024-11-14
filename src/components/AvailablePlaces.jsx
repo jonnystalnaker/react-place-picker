d7ff8efaafbedc16e699b46427f3a242f73f5415
Jonny <jonnystalnaker@gmail.com>
2024-11-14 14:37:20 -0700

    using props to set a default loading text for slow browsers

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+	const [isFetching, setIsFetching] = useState(false);
 	const [availablePlaces, setAvailablePlaces] = useState([]);
 
 	useEffect(() => {
 		async function fetchPlaces() {
+			setIsFetching(true);
 			const response = await fetch('http://localhost:3000/places');
 			const resData = await response.json();
 			setAvailablePlaces(resData.places);
+			setIsFetching(false);
 		}
 
 		fetchPlaces();
@@ -17,8 +20,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
 		<Places
 			title="Available Places"
 			places={availablePlaces}
-			isLoading={false}
-			loadingText="Fetching palce data..."
+			isLoading={isFetching}
+			loadingText="Fetching place data..."
 			fallbackText="No places available."
 			onSelectPlace={onSelectPlace}
 		/>
