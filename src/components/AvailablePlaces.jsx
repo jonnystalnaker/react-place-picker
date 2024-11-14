1db68c1edf192d2abd3bd68ace90d85032212eeb
Jonny <jonnystalnaker@gmail.com>
2024-11-14 14:17:37 -0700

    updating for await async

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -5,13 +5,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
 	const [availablePlaces, setAvailablePlaces] = useState([]);
 
 	useEffect(() => {
-		fetch('http://localhost:3000/places')
-			.then(response => {
-				return response.json();
-			})
-			.then(resData => {
-				setAvailablePlaces(resData.places);
-			});
+		async function fetchPlaces() {
+			const response = fetch('http://localhost:3000/places');
+			const resData = await response.json();
+			setAvailablePlaces(resData.places);
+		}
+
+		fetchPlaces();
 	}, []);
 
 	return (
